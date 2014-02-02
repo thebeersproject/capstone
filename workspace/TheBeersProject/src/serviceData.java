27c994786286afe069eac1d3cae320c131150f26
atwoodni <nick@Nicks-MacBook-Air-i7.local>
2014-02-01 20:57:14 -0800

    Nick: Added databaseObject functionality, examples, and fixed log file parsing so that it currently prints all necessary information (agent, instance, service, service calls, and service time)!

---
+++ workspace/TheBeersProject/src/serviceData.java
@@ -0,0 +1,93 @@
+//package com.hp.iwac.exp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.systemagmbh.components.message.vfei.CSysVfeiMessage;
+import de.systemagmbh.interfaces.message.ISysMessageItem;
+
+public class serviceData {
+	String agentName = null;
+	String instance = null;
+	List<SrvData> services = new ArrayList<SrvData>();
+	String newLine = System.getProperty("line.separator");
+
+	public serviceData(CSysVfeiMessage msg) {
+		//parse this message
+		int size = msg.size();
+		System.out.println("vfie msg size = " + size);
+		for (int x=0; x<size; x++) {
+			ISysMessageItem i = msg.getItem(x);
+			int type = i.getType();
+			String msgName = (String) i.getKey();
+			if ("APPLICATION".equals(msgName)) {
+				agentName = (String)i.getValue();
+			}
+			if ("INSTANCE".equals(msgName)) {
+				instance = (String)i.getValue();
+			}
+			if ("SERVICES".equals(msgName) && i.isList()) {
+				CSysVfeiMessage msg2 = (CSysVfeiMessage) i.getValue();
+				int cnt = 0;
+				boolean itemsExist = true;
+				while (itemsExist) {
+					String serviceName = "SERVICE_" + Integer.toString(cnt);
+					cnt++;
+					CSysVfeiMessage serviceData = null;
+					try {
+						ISysMessageItem i2 = msg2.getItem(serviceName);
+						serviceData = (CSysVfeiMessage) i2.getValue();
+						
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						itemsExist = false;
+					}
+					if (itemsExist) {
+						services.add(new SrvData(serviceData));
+					}
+				}
+
+
+			}
+		}
+	}
+
+	public String toString() {
+		String retString = null;
+		retString = "Agent name = " + agentName + " Instance = " + instance + newLine;
+		for (SrvData s : services) {
+			retString += s.toString();
+			retString += newLine;
+		}
+		return retString;
+		
+	}
+	private class SrvData {
+		String serviceName;
+		Long serviceCnt;
+		Long serviceTotalTime;
+		
+		public SrvData(CSysVfeiMessage srvData) {
+			// srvData should be a list of 5 items, 
+			ISysMessageItem  serviceCallsMsg = null;
+			// System.out.println("srvData is a list message = " + srvData.toString());
+			CSysVfeiMessage info = srvData;
+			// System.out.println("info = " + info.toString());
+			ISysMessageItem serviceMsg = info.getItem("SERVICE_NAME");
+			serviceName = serviceMsg.getValue().toString();
+			
+			serviceCallsMsg = info.getItem("SERVICE_CALLS");
+			serviceCnt = ((Long) serviceCallsMsg.getValue()).longValue();
+			serviceCallsMsg = info.getItem("SERVICE_TIME");
+			serviceTotalTime = ((Long) serviceCallsMsg.getValue()).longValue();
+		}
+		
+		
+		public String toString() {
+			String retString = "Service name = " + serviceName 
+					+ " Count = " + Long.toString(serviceCnt) 
+					+ " Total Time " + Long.toString(serviceTotalTime);
+			return retString;
+		}
+	}
+}
